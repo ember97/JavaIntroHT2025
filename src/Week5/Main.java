eff255ec290cb83251c8a414c8682fcccc5449d5
emmiberge <emmieberger@sogeti.se>
2025-10-06 14:57:26 +0200

    Prep week5

---
+++ src/Week5/Main.java
@@ -0,0 +1,11 @@
+package Week5;
+
+import Week5.Generics.DoubleNode;
+import Week5.Generics.DoublyLinkedList;
+import Week5.Generics.SimplyLinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
